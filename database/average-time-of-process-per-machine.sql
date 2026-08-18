2f07f93062be0a585c0f7c24e3508054daed8865
zia-0825 <ziakhurshid321@gmail.com>
2026-08-19 00:47:23 +0530

    LC-average-time-of-process-per-machine [Runtime: N/A; Memory: N/A]

---
+++ database/average-time-of-process-per-machine.sql
@@ -0,0 +1,8 @@
+SELECT a.machine_id,ROUND(AVG((b.timestamp - a.timestamp)::numeric), 3) AS processing_time
+FROM Activity AS a
+JOIN Activity AS b ON
+a.machine_id = b.machine_id
+AND
+a.process_id =  b.process_id
+WHERE a.activity_type = 'start' AND b.activity_type ='end'
+GROUP BY a.machine_id
