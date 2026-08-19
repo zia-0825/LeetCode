bea4a56751678273ce98fb4820c6d61649e3ca9f
zia-0825 <ziakhurshid321@gmail.com>
2026-08-19 23:02:48 +0530

    LC-average-selling-price [Runtime: N/A; Memory: N/A]

--- database/average-selling-price.sql
+++ database/average-selling-price.sql
@@ -6,7 +6,7 @@ SELECT
         2
     ) AS average_price
 FROM Prices
-JOIN UnitsSold
+LEFT JOIN UnitsSold
     ON Prices.product_id = UnitsSold.product_id
     AND UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date
 GROUP BY Prices.product_id;
