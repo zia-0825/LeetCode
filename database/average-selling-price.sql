c7b4ad65d52d7f25223a29abf3c32568da4bb079
zia-0825 <ziakhurshid321@gmail.com>
2026-08-19 22:59:28 +0530

    LC-average-selling-price [Runtime: N/A; Memory: N/A]

---
+++ database/average-selling-price.sql
@@ -0,0 +1,12 @@
+SELECT 
+    Prices.product_id,
+    ROUND(
+        SUM(UnitsSold.units * Prices.price)::numeric
+        / SUM(UnitsSold.units),
+        2
+    ) AS average_price
+FROM Prices
+JOIN UnitsSold
+    ON Prices.product_id = UnitsSold.product_id
+    AND UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date
+GROUP BY Prices.product_id;
