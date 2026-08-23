7fdec67904b5333e8eedcf86c9d27c1e3bdfcf5c
zia-0825 <ziakhurshid321@gmail.com>
2026-08-23 19:21:50 +0530

    LC-employees-whose-manager-left-the-company [Runtime: N/A; Memory: N/A]

--- database/employees-whose-manager-left-the-company.sql
+++ database/employees-whose-manager-left-the-company.sql
@@ -1,7 +1,8 @@
-SELECT employee_id FROM Employees
-WHERE salary < 30000
-AND manager_id NOT IN
-(SELECT employee_id
+SELECT employee_id
 FROM Employees
+WHERE salary < 30000
+AND manager_id NOT IN (
+    SELECT employee_id
+    FROM Employees
 )
-ORDER BY employee_id
+ORDER BY employee_id;
