aaeff547f88fa65f6c8d81c93d7e55a123b7b8c8
zia-0825 <ziakhurshid321@gmail.com>
2026-08-23 14:35:51 +0530

    LC-biggest-single-number [Runtime: N/A; Memory: N/A]

--- database/biggest-single-number.sql
+++ database/biggest-single-number.sql
@@ -1,5 +1,9 @@
-SELECT MAX(num) AS num FROM (SELECT num FROM MyNumbers
- GROUP BY num
-HAVING COUNT(num)= 1);
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+) AS unique_nums;
 
 
