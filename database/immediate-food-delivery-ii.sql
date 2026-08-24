417715c37b94f193d77a0dc8b07e645f86900abb
zia-0825 <ziakhurshid321@gmail.com>
2026-08-24 20:33:20 +0530

    LC-immediate-food-delivery-ii [Runtime: N/A; Memory: N/A]

--- database/immediate-food-delivery-ii.sql
+++ database/immediate-food-delivery-ii.sql
@@ -1,10 +1,14 @@
-SELECT
-ROUND(SUM(
-    CASE 
-    WHEN (order_date =customer_pref_delivery_date) THEN 1
-    ELSE 0
-    END) *100.00/COUNT(customer_id),2) AS immediate_percentage 
-FROM Delivery 
+SELECT 
+    ROUND(
+        100.0 * SUM(
+            CASE 
+                WHEN order_date = customer_pref_delivery_date THEN 1
+                ELSE 0
+            END
+        ) / COUNT(*),
+        2
+    ) AS immediate_percentage
+FROM Delivery
 WHERE (customer_id, order_date) IN (
     SELECT customer_id, MIN(order_date)
     FROM Delivery
