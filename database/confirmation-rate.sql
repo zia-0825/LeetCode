9866974292cfd7c5ac430510220d1f93b4bf1808
zia-0825 <ziakhurshid321@gmail.com>
2026-08-24 19:15:35 +0530

    LC-confirmation-rate [Runtime: N/A; Memory: N/A]

---
+++ database/confirmation-rate.sql
@@ -0,0 +1,12 @@
+SELECT Signups.user_id, 
+COALESCE(
+SUM(CASE action
+    WHEN 'confirmed' THEN 1
+    ELSE 0
+    END ) *1.0 /NULLIF(COUNT(action), 0),
+    0)
+   AS confirmation_rate
+FROM Confirmations
+RIGHT JOIN Signups ON
+Confirmations.user_id = Signups.user_id    
+GROUP BY Signups.user_id
