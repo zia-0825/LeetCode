876f797e7b8b3fffc9ce3a80bd56c8ae9243c8e4
zia-0825 <ziakhurshid321@gmail.com>
2026-08-24 19:44:41 +0530

    LC-monthly-transactions-i [Runtime: N/A; Memory: N/A]

---
+++ database/monthly-transactions-i.sql
@@ -0,0 +1,12 @@
+ SELECT TO_CHAR(trans_date, 'YYYY-MM') AS month, country, COUNT(amount) AS trans_count,
+ SUM(CASE state
+    WHEN 'approved'THEN 1
+    ELSE 0 
+   END )
+     AS approved_count,
+SUM(amount) AS trans_total_amount,
+SUM(CASE state
+    WHEN 'approved' THEN amount
+    ELSE 0
+   END)  AS approved_total_amount FROM Transactions
+   GROUP BY TO_CHAR(trans_date, 'YYYY-MM'), country
