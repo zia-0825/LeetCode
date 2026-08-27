61f81cd8926196a9c3e5990717d15fbfd61815af
zia-0825 <ziakhurshid321@gmail.com>
2026-08-27 13:05:48 +0530

    LC-customers-who-bought-all-products [Runtime: N/A; Memory: N/A]

--- database/customers-who-bought-all-products.sql
+++ database/customers-who-bought-all-products.sql
@@ -1,3 +1,3 @@
 SELECT customer_id FROM Customer
 GROUP BY customer_id
-HAVING COUNT(DISTINCT(product_key)) = (SELECT COUNT(DISTINCT(product_key)) FROM Customer)
+HAVING COUNT(DISTINCT(product_key)) = (SELECT COUNT(DISTINCT(product_key)) FROM Product)
