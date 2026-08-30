a83d087b453ab5f5cc29cab4334c98f80dc57875
zia-0825 <ziakhurshid321@gmail.com>
2026-08-30 17:00:29 +0530

    LC-last-person-to-fit-in-the-bus [Runtime: N/A; Memory: N/A]

--- database/last-person-to-fit-in-the-bus.sql
+++ database/last-person-to-fit-in-the-bus.sql
@@ -2,18 +2,10 @@ SELECT person_name
 FROM (
     SELECT
         person_name,
-        person_id,
         turn,
-        total_weight,
-        MAX(turn) OVER () AS max_turn
-    FROM (
-        SELECT
-            person_name,
-            person_id,
-            turn,
-            SUM(weight) OVER (ORDER BY turn) AS total_weight
-        FROM Queue
-    ) q1
-    WHERE total_weight <= 1000
-) q2
-WHERE turn = max_turn;
+        SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue
+) q
+WHERE total_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
